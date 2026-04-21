ac861a09c020d4c5d2fea8a8103d3801f33ee591
Medhansh-Xplores <me23btech11046@iith.ac.in>
2026-04-21 08:15:57 +0000

    testing

--- acp-terraform/main.tf
+++ acp-terraform/main.tf
@@ -175,6 +175,7 @@ resource "aws_security_group" "acp_efs" {
 resource "aws_ecr_repository" "acp_backend" {
   name                 = "acp-backend"
   image_tag_mutability = "MUTABLE"
+  force_delete         = true
 
   image_scanning_configuration {
     scan_on_push = true
@@ -186,6 +187,7 @@ resource "aws_ecr_repository" "acp_backend" {
 resource "aws_ecr_repository" "acp_frontend" {
   name                 = "acp-frontend"
   image_tag_mutability = "MUTABLE"
+  force_delete         = true
 
   image_scanning_configuration {
     scan_on_push = true
